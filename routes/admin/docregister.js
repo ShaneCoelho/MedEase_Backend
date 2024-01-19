0f6d8e2d1c3ad27886bfdb2a9eed733b7e21bba3
ShaneCoelho <shanecoelho1@gmail.com>
2024-01-19 19:52:08 +0530

    made use of cloudinary to upload the profile image

--- routes/admin/docregister.js
+++ routes/admin/docregister.js
@@ -3,18 +3,49 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const fetchadmin = require('../../middleware/fetchadmin');
 const router = express.Router();
+const multer = require('multer');
 const Admin = mongoose.model('Admin');
 const Doctor = mongoose.model('Doctor');
+const cloudinary = require('../../helper/imageUpload')
+
+const storage = multer.diskStorage({});
+
+const fileFilter = (req, file, cb) => {
+  console.log(file)
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb('invalid image file!', false);
+  }
+};
+const uploads = multer({ storage, fileFilter });
 
 
-router.post('/adddoctor', fetchadmin, async (req, res) => {
+router.post('/adddoctor', fetchadmin, uploads.single('profile'), async (req, res) => {
+
+    if (!req.user)
+        return res
+            .status(401)
+            .json({ success: false, message: 'unauthorized access!' });
+
     try {
-        const { name, gender, dob, email, phone, address, licene_number, specialization, experience, qualification, med_school, graduation_year, gov_id, med_license, username, password, Avatar } = req.body;
+
+        const jsonData = JSON.parse(req.body.data);
+        const { name, gender, dob, email, phone, address, licene_number, specialization, experience, qualification, med_school, graduation_year, gov_id, med_license, username, password} = jsonData;
 
         const existingUser = await Doctor.findOne({ username });
         if (existingUser) {
             return res.status(409).send({ message: 'Username Already Taken' });
         }
+
+        const result = await cloudinary.uploader.upload(req.file.path, {
+            public_id: `${req.user._id}_profile`,
+            width: 500,
+            height: 500,
+            crop: 'fill',
+        });
+
+        const Avatar=result.url;
 
         const newDoctor = new Doctor({ name, gender, dob, email, phone, address, licene_number, specialization, experience, qualification, med_school, graduation_year, gov_id, med_license, username, password, Avatar });
         await newDoctor.save();
